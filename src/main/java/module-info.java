1229cde351de379d982aa03664be9dedae7f93ca
User <stvigmar@bx.fallmerayer.it>
2023-03-26 17:29:39 +0200

    aaa

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.towerdefensegameinjfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.towerdefensegameinjfx to javafx.fxml;
+    exports com.example.towerdefensegameinjfx.game;
+}
